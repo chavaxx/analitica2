7474c9494634ab0487e6a4eaa61b722852e71f8a
Fernando Jimenez Perera <a01734609@itesm.mx>
2021-05-05 20:01:39 +0000

    sistema para calcular los datos mediante una variable dada

--- ulabox.py
+++ ulabox.py
@@ -15,6 +15,8 @@ with open('ulabox_orders_with_categories_partials_2017.csv', 'r') as file:
         if(row != headers):
             values.append(float(row[X]))
         
+values.sort()
+
 #Cantidad
 cantidad = len(values)
 #Minimo
@@ -36,16 +38,15 @@ for x in values:
 s = math.sqrt((1/(cantidad -1)) * sumatoria)
 
 #posicion
-porcentaje25 = cantidad // 4
-porcentaje50 = cantidad // 2
-porcentaje75 = porcentaje50 + porcentaje25
-porcentaje100 = porcentaje50 * 2
 
-q1 = values[porcentaje25 -1]
-q2 = values[porcentaje50 -1]
-q3 = values[porcentaje75 -1]
-q4 = values[porcentaje100 -1]
+indexq1 = cantidad // 4
+indexq3 = ((3 * cantidad) + 2) // 4
 
+q1 = values[indexq1]
+q2 = mediana
+q3 = values[indexq3]
+q4 = values[cantidad -1]
+iqr = q3 - q1 
 
 print("Variable: %s \t cantida: %d" %(variable, cantidad))
 #print("Variable: %s" %variable)
@@ -54,4 +55,5 @@ print("Media: %d" %media)
 print("Mediana: %d" %mediana)
 print("Moda: %d" %moda)
 print("Desviacion estandar: %d" %s)
-print("Q1: %d \t")
+print("Q1: %d \t Q2: %d \t Q3: %d \t Q4: %d \t" %(q1, q2, q3, q4))
+print("IQR: %d" %iqr)
